9e0de02119fabf208e89a6f13d5a161ccc75cbf3
krati-1993 <kratikavarshney2011@gmail.com>
2016-12-05 15:12:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var name = "Name";
+var age = "Age";
+var dateofbirth = "Date of Birth";
+var placeofbirth = "Place of Birth";
+
+console.log(name +' , '+ age +' , '+ dateofbirth +' and '+ placeofbirth);
